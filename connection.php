1eef0e61b2e9b854d711c2e68d111e79ad7d009d
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-09 14:39:15 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ $password = "000WebHost-root";
 $dbname = "id15358400_uas";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
