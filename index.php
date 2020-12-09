60c6582953e0111a05f14a17e55d747f561f3e2b
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-09 14:23:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
 </head>
 <body>
   <div align="center">
+    <h3>Collaboration Heroku with 000webhost</h3>
+    <?php
+      date_default_timezone_set('Asia/Jakarta');
+      echo date('d F Y H:i:s'); 
+    ?>
     <table border="" width="">
       <thead>
         <tr>
