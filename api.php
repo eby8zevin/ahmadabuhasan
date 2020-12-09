d0c0c1fdf4df2145a051f54ac0ad05fc049dcd35
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-09 16:00:17 +0700

    Create api.php

---
+++ api.php
@@ -0,0 +1,15 @@
+<?php
+include "connection.php";
+
+$query = mysqli_query($conn, "SELECT * FROM tugas_data");
+
+$json = array();
+
+while($row = mysqli_fetch_array($query)){
+	$json[] = $row;
+}
+	
+echo json_encode($json);
+	
+$conn->close();
+?>
