2d34cc98d406679303286c747253b07f86a13bdc
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-09 14:31:33 +0700

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "id15358400_root";
+$password = "000WebHost-root";
+$dbname = "id15358400_uas";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
