5a16889bc488c1c7b1463d3367cec85145776d9e
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-09 14:12:39 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>UAS WEB 7</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+</body>
+</html>
