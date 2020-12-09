ad18f608afaff9907fb8ade673c9ae02a8e6285e
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-09 14:20:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,20 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+  <div align="center">
+    <table border="" width="">
+      <thead>
+        <tr>
+          <th>No</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>No</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
 </body>
 </html>
