0eb434c8463b8fc4f35f56f373040dcc5ad799f2
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-10 15:07:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,25 @@
 </head>
 <body>
   <center><font color="red" size="100"><b>M A I N T E N A N C E</b></font></center>
+  <div align="center">
+    <h3>CRUD Heroku with Postgresql<br>jancukers.com</h3>
+      <?php
+        date_default_timezone_set('Asia/Jakarta');
+        echo date('d F Y H:i:s'); 
+      ?>
+    <table border="1" width="700px">
+      <thead>
+        <tr>
+          <th>No</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>No</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
 </body>
 </html>
