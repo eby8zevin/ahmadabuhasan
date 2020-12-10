89669b7d4b65e6ec60bb93e0ebb21ccf93e53754
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-10 14:35:48 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,11 +1,19 @@
 <?php
-$conn = "pgsql:"
-    . "host=ec2-52-71-55-81.compute-1.amazonaws.com;"
-    . "dbname=d93pbkhncnv410;"
-    . "user=vywyrnwjqzadfe;"
-    . "port=5432;"
-    . "sslmode=require;"
-    . "password=d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a";
+$db = parse_url(getenv("postgres://vywyrnwjqzadfe:d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a@ec2-52-71-55-81.compute-1.amazonaws.com:5432/d93pbkhncnv410"));
 
-$db = new PDO($conn);
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=ec2-52-71-55-81.compute-1.amazonaws.com;port=5432;user=vywyrnwjqzadfe;password=d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a;dbname=d93pbkhncnv410",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+if($pdo) {
+       echo 'connected';
+    } else {
+        echo 'there has been an error connecting';
+    }
+
 ?>
