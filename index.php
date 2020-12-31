c1b06f6520e2adcd4dcb1a71369e03f47af33d1c
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2020-12-31 16:16:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <meta name="description" content="heroku">
   <meta name="author" content="">
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
 </head>
 <body>
   <center><font color="red" size="100"><b>H E R O K U</b></font></center>
