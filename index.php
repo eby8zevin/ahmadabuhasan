d3b80fdc987bc1a93f0e8aaba90bc2a22daa1327
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2020-12-31 16:17:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <meta name="author" content="">
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
 </head>
 <body>
   <center><font color="red" size="100"><b>H E R O K U</b></font></center>
