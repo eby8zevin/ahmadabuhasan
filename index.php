9bbd477c4d539a504feda3c3b8e297a3fef43da3
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-04-05 10:16:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ include "connection.php";
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Heroku">
   <meta name="author" content="AhmadAbuHasan">
-  <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicon/favicon-16x16.png">
-  <link rel="manifest" href="../assets/img/favicon/site.webmanifest">
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="/assets/img/favicon/site.webmanifest">
 </head>
 <body>
   
