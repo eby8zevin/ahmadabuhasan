513649b8069816c72bac49476fa19aff350a39cb
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-04-10 08:11:13 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,10 @@
 $db = parse_url(getenv("postgres://vywyrnwjqzadfe:d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a@ec2-52-71-55-81.compute-1.amazonaws.com:5432/d93pbkhncnv410"));
 
 $pdo = new PDO("pgsql:" . sprintf(
-    "host=ec2-52-71-55-81.compute-1.amazonaws.com;port=5432;user=vywyrnwjqzadfe;password=d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a;dbname=d93pbkhncnv410",
+    "host=ec2-52-71-55-81.compute-1.amazonaws.com;
+     port=5432;user=vywyrnwjqzadfe;
+     password=d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a;
+     dbname=d93pbkhncnv410",
     $db["host"],
     $db["port"],
     $db["user"],
