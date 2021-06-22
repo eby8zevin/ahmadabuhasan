b3dcce648c62be789ca04c43ef1f627bf517ccd3
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-06-22 15:38:35 +0700

    Database Credentials

--- connection.php
+++ connection.php
@@ -1,23 +1,26 @@
 <?php
-$db = parse_url(getenv("postgres://vywyrnwjqzadfe:d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a@ec2-52-71-55-81.compute-1.amazonaws.com:5432/d93pbkhncnv410"));
 
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=ec2-52-71-55-81.compute-1.amazonaws.com;
-     port=5432;user=vywyrnwjqzadfe;
-     password=d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a;
-     dbname=d93pbkhncnv410",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
+$host = "ec2-52-71-55-81.compute-1.amazonaws.com";
+$dbname = "d93pbkhncnv410";
+$user = "vywyrnwjqzadfe";
+$port = "5432";
+$password = "d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a";
 
-// Check connection
-if(!$pdo) { 
-    echo 'There has been an error connecting';
-} else {
-    echo 'Connected successfully';
+try{
+    //Set DSN Data Source Name
+    $dsn = "pgsql:host" . $host . ";
+                  port=" . $port . ";
+                  dbname=" . $dbname . ";
+                  user=" . $user . ";
+                  password=" . $password . ";";
+    
+    // Create a PDO Instance
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $e) {
+    echo 'Connection Failed: ' . $e->getMessage();
 }
 
 ?>
