5a9020f5d7f17027135325cdaec6c42305aba9ba
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-09-01 05:25:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <title>UAS WEB 7 - Heroku</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Heroku">
+  <meta name="description" content="HerokuPostgreSQL">
   <meta name="author" content="AhmadAbuHasan">
 
   <link rel="apple-touch-icon" sizes="180x180" href="./assets/img/favicon/apple-touch-icon.png">
