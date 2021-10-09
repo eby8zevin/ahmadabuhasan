e5eef39abaea7404eda3dc555d86f6a8c53536d4
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-10-10 05:46:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,15 +25,6 @@
       ?></p>
   </div>
   
-  <?php
-  $query = "SELECT * FROM tugas_data; 
-  $rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
-  while ($row = pg_fetch_row($rs)) {
-  echo "$row[0]";
-  }
-  pg_close($con); 
-  ?>
-  
   <footer align="center">
     <?php echo "<p>Copyright &copy; 1945-" . date("Y") ;?>
     <a href="https://ahmadabuhasan.me">ahmadabuhasan.me</a>
