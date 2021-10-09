48f6975dea10914cf55b49861067497d0d55ba25
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-10-10 05:33:42 +0700

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+$host = "ec2-52-71-55-81.compute-1.amazonaws.com";
+$dbname = "d93pbkhncnv410";
+$user = "vywyrnwjqzadfe";
+$password = "d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a";
+$port = "5432";
+
+$con = "host=$host; dbname=$dbname; user=$user; password=$password; port=$port; sslmode=require";
+
+if (!$con)
+{
+  echo "Database connection failed.";
+}else{
+  echo "Database connection success.";
+}
+?>
