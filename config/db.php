5e9454d1b0cddeeac52c1645c1b6c4b4ee1d4011
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-10-10 05:44:07 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@ $user = "vywyrnwjqzadfe";
 $password = "d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a";
 $port = "5432";
 
-$con = "host=$host; dbname=$dbname; user=$user; password=$password; port=$port; sslmode=require";
+$con = pg_connect("host=$host dbname=$dbname user=$user password=$password port=$port sslmode=require");
 
 if (!$con)
 {
