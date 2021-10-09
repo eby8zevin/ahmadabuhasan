a0121766ad53d9679dfa751f6531fd0e55e95c3a
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-10-10 05:45:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
   $query = "SELECT * FROM tugas_data; 
   $rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
   while ($row = pg_fetch_row($rs)) {
-  echo "$row[0] $row[1] $row[2]\n";
+  echo "$row[0]";
   }
   pg_close($con); 
   ?>
