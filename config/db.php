f3e782e67f2e978fa61afb107528e39b1a63c068
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-12-21 20:55:16 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "ec2-52-71-55-81.compute-1.amazonaws.com";
-$dbname = "d93pbkhncnv410";
-$user = "vywyrnwjqzadfe";
-$password = "d1ec31b2868f9070413367ba19c26f79b439365709fb6a90f6e5fb227cc8830a";
+$host = "ec2-34-192-246-6.compute-1.amazonaws.com";
+$dbname = "d4rp9f2acpmucp";
+$user = "mncuhzmbyxppeg";
+$password = "f09da77418af3b555fbebb9fb1a74a568bdd6421c4f70f955d9a6f285c31a838";
 $port = "5432";
 
 $con = pg_connect("host=$host dbname=$dbname user=$user password=$password port=$port sslmode=require");
