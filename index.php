1ad8de958ee478e44d55fac011547fe5510e4fce
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-03-09 23:58:43 +0700

    add Navigation

--- index.php
+++ index.php
@@ -33,15 +33,52 @@
                 <div class="container">
                     <div class="row">
                         <div class="intro-text">
-                            <h1>Ahmad Abu Hasan</h1>
+                            <h1>Hello, I'm <span class"name">Ahmad Abu Hasan</span></h1>
                             <p>Android Developer</p>
-                            <a href="#about" class="btn btn-custom btn-lg page-scroll">About Me</a>
+                            <a href="#about" class="btn btn-default btn-lg page-scroll">About Me</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </header>
+
+    <!-- Navigation -->
+    <div id="nav">
+        <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="#page-top">
+                        <i class="fa fa-play-circle"></i> <span class="light">Ahmad Abu</span> Hasan
+                    </a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                    <ul class="nav navbar-nav">
+                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                        <li class="hidden">
+                            <a href="#page-top"></a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#about">About</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#portfolio">Portfolio</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+    </div>
 
     </body>
 </html>
