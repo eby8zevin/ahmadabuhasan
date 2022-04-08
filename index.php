66e98b07b4f595b127acaf8c4beca91e6d59f222
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-04-08 21:24:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -167,7 +167,7 @@
             <div class="row">
                 <div class="col-md-3 col-sm-3 wow fadeInDown" data-wow-delay="200ms">
                     <div class="achievement-box">
-                        <span class="count">5452</span>
+                        <span class="count">5453</span>
                         <h4>Total Contributions</h4>
                     </div>
                 </div>
