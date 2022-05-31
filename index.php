5f11f70171c3e7ba757e2985086581fd6fac9082
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-05-31 22:40:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -296,7 +296,7 @@
             <div class="row">
                 <div class="col-md-3 col-sm-3 wow fadeInDown" data-wow-delay="200ms">
                     <div class="achievement-box">
-                        <span class="count">6369</span>
+                        <span class="count">6405</span>
                         <h4>Total Contributions</h4>
                     </div>
                 </div>
