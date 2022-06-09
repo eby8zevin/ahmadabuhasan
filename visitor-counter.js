bedb01f52687946b7ae059150c3a8d7174d9711c
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-06-09 22:07:45 +0700

    visit count js

--- visitor-counter.js
+++ visitor-counter.js
@@ -3,7 +3,7 @@ var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
 
 // Check if page_view entry is present
-if (visitCount === null) {
+if (visitCount) {
     visitCount = Number(visitCount) + 1;
     localStorage.setItem("page_view", visitCount);
 } else {
