15c9b7a5f162418db02fdbdcbdf53444ac97ad97
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-15 14:10:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -343,13 +343,13 @@
                 </div>
                 <div class="col-md-3 col-sm-3 wow fadeInDown" data-wow-delay="400ms">
                     <div class="achievement-box">
-                        <span class="count">137</span>
+                        <span class="count">138</span>
                         <h4>Current Streak</h4>
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-3 wow fadeInDown" data-wow-delay="600ms">
                     <div class="achievement-box">
-                        <span class="count">137</span>
+                        <span class="count">138</span>
                         <h4>Longest Streak</h4>
                     </div>
                 </div>
