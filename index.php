5a10b2e9186690c806bf2d87f3a34735b694a725
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-08-02 22:40:32 +0700

    Update initial-scale

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=0.1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Portfolio">
     <meta name="author" content="Ahmad Abu Hasan">
     <meta name="keywords" content="Portfolio, Developer, Android, Java, XML, Kotlin, HTML, CSS, JavaScript, PHP, MySQL">
